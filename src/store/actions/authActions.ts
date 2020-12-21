353fd7b24aa476621bf48cd21aebfe62b3dcd98c
IlyaAvex <15162342h@gmail.com>
2020-12-22 01:38:43 +0800

    added new actions in app/store/ for login form

--- src/store/actions/authActions.ts
+++ src/store/actions/authActions.ts
@@ -1,13 +1,57 @@
-import { LOGIN_USER } from '../types';
+import {
+  FETCH_USERS,
+  SET_CURRENT_USER,
+  UNSET_CURRENT_USER,
+  SET_USER_ISLOGGED,
+  UNSET_USER_ISLOGGED
+} from '../types';
 
-interface LoginUser {
+interface IUser {
+  id: string;
   loginForm: string;
   passForm: string;
 }
 
-export function loginUser(payload: LoginUser) {
+export function setCurrentUser(payload: IUser) {
   return {
-    type: LOGIN_USER,
+    type: SET_CURRENT_USER,
     payload
+  };
+}
+
+export function unSetCurrentUser() {
+  return {
+    type: UNSET_CURRENT_USER
+  };
+}
+
+export function fetchUser() {
+  return async function(dispatch: any) {
+    try {
+      const url = 'http://localhost:3001/users';
+
+      const res = await fetch(url);
+      const json = await res.json();
+
+      dispatch({
+        type: FETCH_USERS,
+        payload: json
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+}
+
+export function setUserIsLogged() {
+  return {
+    type: SET_USER_ISLOGGED
+  };
+}
+
+export function unSetUserIsLogged() {
+  return function(dispatch: any) {
+    dispatch({ type: UNSET_USER_ISLOGGED });
+    dispatch(unSetCurrentUser());
   };
 }
