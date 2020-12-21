36d2f8ed64b2d8da080ceebfde0fd2b4129143b9
IlyaAvex <15162342h@gmail.com>
2020-12-21 21:57:51 +0800

    include redux in app/index. create store.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Index from './app';
+import App from './app';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+import GlobalStyle from './globalStyle';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { rootReducer } from './store/rootReducer';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
