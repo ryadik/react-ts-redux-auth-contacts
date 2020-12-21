0e85a4423ac5c0ec31e9c71f32175e6926f31d3c
IlyaAvex <15162342h@gmail.com>
2020-12-22 01:41:19 +0800

    added login functional to App component in app/ moved App from FC to PureC

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,101 @@
 import * as React from 'react';
 
-import { useSelector } from 'react-redux';
-
 import { RootState } from '../store/rootReducer';
+import {
+  fetchUser,
+  setUserIsLogged,
+  unSetUserIsLogged
+} from '../store/actions/authActions';
 
 import LoginForm from './components/LoginForm';
 
 import { Container } from './style';
+import { connect } from 'react-redux';
 
-function App() {
-  const user = useSelector((state: RootState) => state.auth.user);
+class App extends React.PureComponent<IProps, any> {
+  constructor(props: IProps) {
+    super(props);
+  }
 
-  console.log('user: ', user);
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-  return (
-    <Container>
-      <LoginForm />
-    </Container>
-  );
+  checkToUserExisting = (usersArr: IUser[], userItem: IUser): void => {
+    console.log('userItem', userItem);
+
+    for (let i = 0; i < usersArr.length; i++) {
+      console.log('итерация: ', i);
+      console.log('usersArr ', usersArr[i]);
+
+      if (usersArr[i].login !== userItem.login) {
+        continue;
+      }
+
+      if (usersArr[i].password !== userItem.password) {
+        continue;
+      }
+
+      console.log('successful');
+      this.props.setUserIsLogged();
+      break;
+    }
+  };
+
+  render() {
+    const { user, fetchedUsers, isLogged, unSetUserIsLogged } = this.props;
+
+    if (isLogged) {
+      return (
+        <Container>
+          <div>
+            <h1>Вошел пользователь</h1>
+            <p>Под ником: {user.login}</p>
+            <p>Пароль: {user.password}</p>
+            <button onClick={unSetUserIsLogged}>Выйти</button>
+          </div>
+        </Container>
+      );
+    }
+
+    if (!isLogged) {
+      this.checkToUserExisting(fetchedUsers, user);
+
+      return (
+        <Container>
+          <LoginForm />
+        </Container>
+      );
+    }
+  }
 }
 
-export default App;
+interface IProps {
+  user: IUser;
+  fetchedUsers: IUser[];
+  isLogged: boolean;
+
+  fetchUser: any;
+  setUserIsLogged: any;
+  unSetUserIsLogged: any;
+}
+
+interface IUser {
+  id: string;
+  login: string;
+  password: string;
+}
+
+const mapStateToProps = (state: RootState) => ({
+  user: state.auth.user,
+  fetchedUsers: state.auth.fetchedUsers,
+  isLogged: state.auth.isLogged
+});
+
+const mapDispatchToProps = {
+  fetchUser,
+  setUserIsLogged,
+  unSetUserIsLogged
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
