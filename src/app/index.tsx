ecc9f2b8e508acf9e4461d2126e5799fe7a4d638
IlyaAvex <15162342h@gmail.com>
2020-12-21 22:03:15 +0800

    changed App component in app/

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import * as React from 'react';
+
+import { useSelector } from 'react-redux';
+
+import { RootState } from '../store/rootReducer';
+
+import LoginForm from './components/LoginForm';
 
 import { Container } from './style';
 
-function Index() {
-  return <Container />;
+function App() {
+  const user = useSelector((state: RootState) => state.auth.user);
+
+  console.log('user: ', user);
+
+  return (
+    <Container>
+      <LoginForm />
+    </Container>
+  );
 }
 
-export default Index;
+export default App;
--- src/app/style.ts
+++ src/app/style.ts
@@ -2,6 +2,14 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   max-width: 1330px;
-  margin: 0 auto;
   width: 100%;
+  min-height: 100vh;
+
+  font-family: 'Nunito', sans-serif;
+
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
