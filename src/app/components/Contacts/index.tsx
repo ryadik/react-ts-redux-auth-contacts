ab2112795da68e98732bc82e0d1f7324043d8c9f
IlyaAvex <15162342h@gmail.com>
2020-12-24 14:01:43 +0800

    added search functional in Contacts component in app/

--- src/app/components/Contacts/index.tsx
+++ src/app/components/Contacts/index.tsx
@@ -3,18 +3,59 @@ import { ContactsWrapper } from './style';
 import ContactsItem from './ContactsItem';
 import { RootState } from '../../../store/rootReducer';
 import { connect } from 'react-redux';
+import { Input } from '../../styledComponents/Input';
 
-class Contacts extends React.PureComponent<IProps, any> {
+class Contacts extends React.PureComponent<IProps, IState> {
   constructor(props: IProps) {
     super(props);
   }
 
+  state: IState = {
+    searchValue: ''
+  };
+
+  searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value.trim()
+    }));
+  };
+
+  filterArray = (array: IContacts[], filterParam: string) => {
+    return array.filter(
+      item =>
+        item.name.toLowerCase().search(filterParam) > -1 ||
+        item.descr.toLowerCase().search(filterParam) > -1
+    );
+  };
+
   render() {
     const { contacts, activeContact } = this.props;
+    const { searchValue } = this.state;
+
+    const filteredContacts = searchValue
+      ? this.filterArray(contacts, searchValue.trim().toLowerCase())
+      : contacts;
 
     return (
       <ContactsWrapper>
-        {contacts.map(item => (
+        <header>
+          <div className="top">
+            <h2>Контакты</h2>
+          </div>
+
+          <div className="bottom">
+            <Input
+              type="text"
+              placeholder="Поиск"
+              name="searchValue"
+              value={searchValue}
+              onChange={this.searchHandler}
+            />
+          </div>
+        </header>
+
+        {filteredContacts.map(item => (
           <ContactsItem
             key={item.id}
             data={item}
@@ -31,14 +72,20 @@ const mapStateToProps = (state: RootState) => ({
   activeContact: state.auth.activeContact
 });
 
+interface IContacts {
+  id: number;
+  name: string;
+  descr: string;
+  imgPath: string;
+}
+
 interface IProps {
-  contacts: {
-    id: number;
-    name: string;
-    descr: string;
-    imgPath: string;
-  }[];
+  contacts: IContacts[];
   activeContact: number | null;
+}
+
+interface IState {
+  searchValue: string;
 }
 
 export default connect(mapStateToProps)(Contacts);
--- src/app/components/Contacts/style.ts
+++ src/app/components/Contacts/style.ts
@@ -1,5 +1,44 @@
 import styled from 'styled-components';
+import { Input } from '../../styledComponents/Input';
+import { shadow } from '../../variables';
 
 export const ContactsWrapper = styled.div`
   width: 30%;
+
+  header {
+    margin-bottom: 15px;
+    border-bottom: 1px solid #ec008c;
+
+    div.top {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-bottom: 1px solid #ec008c;
+
+      & > h2 {
+        font-size: 22px;
+        line-height: 29px;
+        font-weight: bold;
+        color: #000;
+      }
+    }
+
+    div.bottom {
+      ${Input} {
+        width: 100%;
+        height: 40px;
+
+        box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+
+        transition: box-shadow 0.3s linear;
+
+        &:active,
+        &:focus {
+          box-shadow: ${shadow};
+        }
+      }
+
+      padding: 10px 0;
+    }
+  }
 `;
