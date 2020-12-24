ac39637a479ba91bc32e7ffdf30c35e3f5d2e388
IlyaAvex <15162342h@gmail.com>
2020-12-24 18:15:19 +0800

    added new variables in app/

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -5,6 +5,11 @@ export const shadowBtn = 'drop-shadow(2px 2px 10px rgba(203, 127, 127, 0.5))';
 
 // gradients
 
+export const greenGradient90 =
+  'linear-gradient(98.48deg, #2AF598 -3.21%, #009EFD 85.11%)';
+export const greenGradient180 =
+  'linear-gradient(180deg, #2AF598 -3.21%, #009EFD 85.11%)';
+
 export const redGradient90 = 'linear-gradient(90deg, #ec008c 0%, #fc6767 100%)';
 export const redGradient180 =
   'linear-gradient(180deg, #ec008c 0%, #fc6767 100%)';
