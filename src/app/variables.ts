ecad713549eb4bda48b2e143e4b0cb275ba990aa
K1nGsmaN-hub <15162342h@gmail.com>
2020-12-27 02:56:58 +0800

    added new variables in app/

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -2,18 +2,12 @@
 
 export const shadow = '0px 0px 20px rgba(0, 0, 0, 0.13)';
 export const shadowBtn = 'drop-shadow(2px 2px 10px rgba(203, 127, 127, 0.5))';
-export const blueShadowBtn =
-  'drop-shadow(2px 2px 10px rgba(127, 157, 203, 0.5))';
 
 // gradients
 
 export const greenGradient90 =
-  'linear-gradient(98.48deg, #2AF598 0%, #009EFD 85%)';
-export const greenGradient180 =
-  'linear-gradient(180deg, #2AF598 0%, #009EFD 85%)';
+  'linear-gradient(90deg, #44BB9A 9%, #8DE2CB 100%)';
 
-export const blueGradient180 =
-  'linear-gradient(180deg, #4481EB 0%, #04BEFE 140%)';
 export const pinkGradient90 =
   'linear-gradient(90deg, #f54ea2 13%, #ff7676 110%)';
 
@@ -23,3 +17,8 @@ export const redGradient180 =
 
 export const grayFade =
   'linear-gradient(90deg, #e7e7e7 0%, rgba(0, 0, 0, 0) 88.75%)';
+
+
+// other
+
+export const url = 'http://localhost:3001/';
