d25ece7dc2d028c76195d8088179c1b1710798b1
K1nGsmaN-hub <15162342h@gmail.com>
2020-12-27 02:58:00 +0800

    edited Input sc in app/

--- src/app/styledComponents/Input.ts
+++ src/app/styledComponents/Input.ts
@@ -9,13 +9,21 @@ export const Input = styled.input`
   border-radius: 10px;
 
   background-color: #fff;
-  box-shadow: ${shadow};
 
   font-size: 16px;
   line-height: 22px;
   color: #000;
 
   box-sizing: border-box;
+
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+
+  transition: box-shadow 0.3s linear;
+
+  &:active,
+  &:focus {
+    box-shadow: ${shadow};
+  }
 
   ::placeholder {
     color: #646464;
