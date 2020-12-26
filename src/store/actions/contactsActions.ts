f0ae9e4e5ebe8bfede89b2b356ad4f9fbaf0dec8
K1nGsmaN-hub <15162342h@gmail.com>
2020-12-27 02:54:25 +0800

    added new action for contacts in store/

--- src/store/actions/contactsActions.ts
+++ src/store/actions/contactsActions.ts
@@ -1,4 +1,11 @@
-import { GET_CONTACTS, SET_ACTIVE_CONTACT } from '../types';
+import { url } from '../../app/variables';
+import {
+  ADD_CONTACT,
+  GET_CONTACTS,
+  SET_ACTIVE_ADD_CONTACT_FORM,
+  SET_ACTIVE_CONTACT,
+  UNSET_ACTIVE_ADD_CONTACT_FORM
+} from '../types';
 
 export function getContacts(contacts: IContacts[]) {
   return {
@@ -14,9 +21,55 @@ export function setActiveContact(id: number) {
   };
 }
 
+export function setActiveContactForm() {
+  return {
+    type: SET_ACTIVE_ADD_CONTACT_FORM
+  };
+}
+
+export function unSetActiveContactForm() {
+  return {
+    type: UNSET_ACTIVE_ADD_CONTACT_FORM
+  };
+}
+
+export function addContact(userName: string, contact: IContacts) {
+  return async function(dispatch: any) {
+    try {
+      const users = await fetch(`${url}users`);
+      const usersParsed: IFetchedUser[] = await users.json();
+      const filteredUser = usersParsed.filter(item => item.login === userName);
+
+      filteredUser[0].contacts.push(contact);
+
+      const res = await fetch(`${url}users/${filteredUser[0].id}`, {
+        method: 'PATCH',
+        body: JSON.stringify(filteredUser[0]),
+        headers: { 'Content-Type': 'application/json' }
+      });
+
+      if (res.ok) {
+        dispatch({
+          type: ADD_CONTACT,
+          payload: contact
+        });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+}
+
 interface IContacts {
   id: number;
   name: string;
   decr: string;
   imgPath: string;
+}
+
+interface IFetchedUser {
+  id: string;
+  login: string;
+  password: string;
+  contacts: IContacts[];
 }
