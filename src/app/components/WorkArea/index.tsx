e58f55a39f6729aa09cb560a0f085832ef252c14
K1nGsmaN-hub <15162342h@gmail.com>
2020-12-28 03:12:32 +0800

    added WorkArea component in app/

--- src/app/components/WorkArea/index.tsx
+++ src/app/components/WorkArea/index.tsx
@@ -1,8 +1,58 @@
 import * as React from 'react';
-import { WorkAreaWrapper } from './style';
+import { connect } from 'react-redux';
+import { RootState } from '../../../store/rootReducer';
 
-const WorkArea: React.FC = () => {
-  return <WorkAreaWrapper>work area</WorkAreaWrapper>;
-};
+import { setActiveContact } from '../../../store/actions/contactsActions';
 
-export default WorkArea;
+import SelectedContact from './SelectedContact';
+
+import { WorkAreaWrapper, EmptyArea } from './style';
+
+class WorkArea extends React.PureComponent<IProps, any> {
+  constructor(props: IProps) {
+    super(props);
+  }
+
+  render() {
+    const { activeContact, contacts } = this.props;
+
+    if (!activeContact) {
+      return (
+        <EmptyArea>
+          <span>Выберите контакт</span>
+        </EmptyArea>
+      );
+    }
+
+    return (
+      <WorkAreaWrapper>
+        {contacts !== undefined &&
+          contacts.map(item => {
+            if (item.id === activeContact) {
+              return <SelectedContact key={item.id} data={item} />;
+            }
+          })}
+      </WorkAreaWrapper>
+    );
+  }
+}
+
+const mapStateToProps = (state: RootState) => ({
+  contacts: state.contacts.contacts,
+  activeContact: state.contacts.activeContact
+});
+
+interface IContacts {
+  id: string;
+  name: string;
+  descr: string;
+  imgPath: string;
+}
+
+// without "?" don't work, idk why
+interface IProps {
+  contacts?: IContacts[];
+  activeContact?: string | null;
+}
+
+export default connect(mapStateToProps, null)(WorkArea);
--- src/app/components/WorkArea/style.ts
+++ src/app/components/WorkArea/style.ts
@@ -3,8 +3,20 @@ import { shadow } from '../../variables';
 
 export const WorkAreaWrapper = styled.div`
   width: 70%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   box-shadow: ${shadow};
   border-radius: 10px;
   padding: 20px;
   box-sizing: border-box;
+`;
+
+export const EmptyArea = styled.div`
+  width: 70%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
