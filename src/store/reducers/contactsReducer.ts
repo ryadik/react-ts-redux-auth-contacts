959fff50c21fb94e627e204426a80ae6b328c154
K1nGsmaN-hub <15162342h@gmail.com>
2021-01-03 15:51:01 +0800

    added reducers to contactsReducer in store/

--- src/store/reducers/contactsReducer.ts
+++ src/store/reducers/contactsReducer.ts
@@ -1,16 +1,19 @@
 import {
   ADD_CONTACT,
   DELETE_CONTACT,
+  DISABLE_EDIT_MODE,
+  ENABLE_EDIT_MODE,
   GET_CONTACTS,
   SET_ACTIVE_ADD_CONTACT_FORM,
   SET_ACTIVE_CONTACT,
-  UNSET_ACTIVE_ADD_CONTACT_FORM
-} from '../types';
+  UNSET_ACTIVE_ADD_CONTACT_FORM, UPDATE_CONTACT
+} from "../types";
 
 const initialState: IState = {
   contacts: [],
   activeContact: null,
-  activeAddContactForm: false
+  activeAddContactForm: false,
+  editMode: false
 };
 
 export const contactsReducer = (state = initialState, action: IAction) => {
@@ -45,11 +48,30 @@ export const contactsReducer = (state = initialState, action: IAction) => {
         contacts: [...state.contacts, action.payload]
       };
 
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: action.payload
+      };
+
     case DELETE_CONTACT:
       return {
         ...state,
         contacts: action.payload
       };
+
+    case ENABLE_EDIT_MODE:
+      return {
+        ...state,
+        editMode: true
+      };
+
+    case DISABLE_EDIT_MODE:
+      return {
+        ...state,
+        editMode: false
+      };
+
     default:
       return state;
   }
@@ -71,4 +93,5 @@ interface IState {
   contacts: IContacts[];
   activeContact: string | null;
   activeAddContactForm: boolean;
+  editMode: boolean;
 }
