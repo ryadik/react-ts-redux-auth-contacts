cbb91e320c0fcaf3f7139503dfeb79339eda13b4
K1nGsmaN-hub <15162342h@gmail.com>
2021-01-03 15:48:36 +0800

    added EditMode component in WorkArea/SelectedContact

---
+++ src/app/components/WorkArea/SelectedContact/EditMode/index.tsx
@@ -0,0 +1,129 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+
+import {
+  disableEditMode,
+  updateContact
+} from '../../../../../store/actions/contactsActions';
+
+import { EditModeWrapper } from './style';
+
+import { Input } from '../../../../styledComponents/Input';
+import { Button } from '../../../../styledComponents/Button';
+import { RootState } from '../../../../../store/rootReducer';
+
+class EditMode extends React.PureComponent<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+  }
+
+  state: IState = {
+    nameForm: '',
+    descrForm: '',
+    imgPathForm: ''
+  };
+
+  submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { data, userName, updateContact, disableEditMode } = this.props;
+
+    const { nameForm, descrForm, imgPathForm } = this.state;
+
+    const newContact = {
+      id: data.id,
+      name: nameForm || data.name,
+      descr: descrForm || data.descr,
+      imgPath: imgPathForm || data.imgPath
+    };
+
+    updateContact(userName, newContact);
+    disableEditMode();
+  };
+
+  changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  render() {
+    const {
+      data: { name, descr, imgPath }
+    } = this.props;
+    const { nameForm, descrForm, imgPathForm } = this.state;
+
+    return (
+      <EditModeWrapper>
+        <h2>Редактирование контакта</h2>
+        <p>
+          Если Вы не хотетите изменять некоторые из полей, оставьте их пустыми.
+        </p>
+
+        <form onSubmit={this.submitHandler}>
+          <Input
+            type="text"
+            placeholder={name}
+            name="nameForm"
+            value={nameForm}
+            onChange={this.changeHandler}
+          />
+          <Input
+            type="text"
+            placeholder={descr}
+            name="descrForm"
+            value={descrForm}
+            onChange={this.changeHandler}
+          />
+          <Input
+            type="text"
+            placeholder={imgPath}
+            name="imgPathForm"
+            value={imgPathForm}
+            onChange={this.changeHandler}
+          />
+
+          <div>
+            <Button type="submit" className="submit">
+              Создать
+            </Button>
+            <Button type="button" onClick={this.props.disableEditMode}>
+              Отмена
+            </Button>
+          </div>
+        </form>
+      </EditModeWrapper>
+    );
+  }
+}
+
+interface IProps {
+  data: {
+    id: string;
+    name: string;
+    descr: string;
+    imgPath: string;
+  };
+
+  userName: string;
+  disableEditMode: any;
+  updateContact: any;
+}
+
+interface IState {
+  nameForm: string;
+  descrForm: string;
+  imgPathForm: string;
+}
+
+const mapStateToProps = (state: RootState) => ({
+  userName: state.auth.user.login
+});
+
+const mapDispatchToProps = {
+  disableEditMode,
+  updateContact
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditMode);
---
+++ src/app/components/WorkArea/SelectedContact/EditMode/style.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import { Button } from '../../../../styledComponents/Button';
+import { Input } from '../../../../styledComponents/Input';
+import { greenGradient90, redGradient90, shadow } from '../../../../variables';
+
+export const EditModeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h3 {
+    font-size: 22px;
+    line-height: 27px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  p {
+    max-width: 250px;
+    font-size: 16px;
+    line-height: 19px;
+    color: #a09e9e;
+
+    margin-bottom: 15px;
+  }
+
+  form {
+    width: 250px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    ${Input} {
+      height: 50px;
+      margin-bottom: 15px;
+    }
+
+    & > div {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+
+      ${Button} {
+        width: 48%;
+        height: 45px;
+
+        filter: unset;
+        box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+        background: ${redGradient90} no-repeat;
+        background-size: 0px;
+        color: #000;
+
+        transition: background-size 0.2s linear, color 0.2s linear,
+          box-shadow 0.2s linear;
+
+        &:hover {
+          box-shadow: ${shadow};
+          background-size: 100%;
+          color: #fff;
+        }
+
+        &.submit {
+          background: ${greenGradient90} no-repeat;
+          background-size: 100%;
+          color: #fff;
+        }
+      }
+    }
+  }
+`;
