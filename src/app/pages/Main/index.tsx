5c5394d8aa8d1f444ab6e83a61492162acaaab15
K1nGsman-hub <15162342h@gmail.com>
2021-01-09 19:37:11 +0800

    added close button (exit btn) in app/pages/Main

--- src/app/pages/Main/index.tsx
+++ src/app/pages/Main/index.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
 
+import { unSetUserIsLogged } from '../../../store/actions/authActions';
+
 import Contacts from '../../components/Contacts';
 import WorkArea from '../../components/WorkArea';
 
 import { MainWrapper } from './style';
+import { connect } from 'react-redux';
 
-class Main extends React.PureComponent<any, any> {
-  constructor(props: any) {
+class Main extends React.PureComponent<IProps, any> {
+  constructor(props: IProps) {
     super(props);
   }
 
   render() {
     return (
       <MainWrapper>
+        <button
+          className="close-btn"
+          title="Выход"
+          onClick={this.props.unSetUserIsLogged}
+        />
         <Contacts />
         <div className="divider" />
         <WorkArea />
@@ -21,4 +29,12 @@ class Main extends React.PureComponent<any, any> {
   }
 }
 
-export default Main;
+interface IProps {
+  unSetUserIsLogged: any;
+}
+
+const mapDispatchToProps = {
+  unSetUserIsLogged
+};
+
+export default connect(null, mapDispatchToProps)(Main);
--- src/app/pages/Main/style.ts
+++ src/app/pages/Main/style.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import { shadow } from '../../variables';
 
 export const MainWrapper = styled.div`
+  position: relative;
+
   width: 100%;
   height: 705px;
   background-color: #fff;
@@ -14,6 +16,29 @@ export const MainWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  button.close-btn {
+    display: block;
+    position: absolute;
+    top: 15px;
+    left: 15px;
+
+    padding: 0;
+    border: 1px solid #ec4c45;
+    background-color: #ff6158;
+
+    width: 15px;
+    height: 15px;
+
+    border-radius: 100%;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+
+    transition: background-color 0.15s linear;
+
+    &:hover {
+      background-color: #ec4c45;
+    }
+  }
 
   & > div {
     height: 100%;
